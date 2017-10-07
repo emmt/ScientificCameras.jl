9fe8b42f460ef24ff5b66349a0af2bed417e635e
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2017-10-07 17:19:18 +0200

    Update and fix doc.

--- src/types.jl
+++ src/types.jl
@@ -5,10 +5,9 @@
 #
 #------------------------------------------------------------------------------
 #
-# This file is part of the `Phoenix.jl` package which is licensed under the MIT
-# "Expat" License.
+# This file is part of the `ScientificCameras.jl` package which is licensed
+# under the MIT "Expat" License.
 #
-# Copyright (C) 2016, Éric Thiébaut & Jonathan Léger.
 # Copyright (C) 2017, Éric Thiébaut.
 #
 
@@ -16,44 +15,33 @@
 
 Concrete types derived from abstract type `ScientificCamera` are used to
 uniquely identify the different camera models or different combinations of
-camera and frame grabber.
+camera model and frame grabber.
 
 """
 abstract type ScientificCamera end
 
 """
 
-`Phoenix.Configuration` is a structure to store acquisition parameters.
+`ROI` is a structure to store a region of interest (ROI), its fields are:
 
-For now, each acquisition buffer stores a single image (of size `roi_width` by
-`roi_height` pixels).  All acquisition buffers have `buf_stride` bytes per line
-and a number of lines greater of equal the height of the images.
+- `xoff`:   Horizontal offset of the ROI relative to detector.
+- `yoff`:   Vertical offset of the ROI relative to detector.
+- `width`:  Width of the ROI.
+- `height`: Height of the ROI.
 
-The following fields are available:
+All fields are integer valued (of type `Int`) and given in pixels.  A ROI is
+constructed by:
 
-- `roi_width`:  Width of ROI (in pixels, integer).
-- `roi_height`: Height of ROI (in pixels, integer).
-- `cam_depth`:  Bits per pixel of the camera (integer).
-- `cam_xoff`:   Horizontal offset of ROI relative to detector (in pixels, integer).
-- `cam_yoff`:   Vertical offset of ROI relative to detector (in pixels, integer).
-- `buf_number`  Number of acquisition buffers (integer).
-- `buf_xoff`:   Horizontal offset of ROI relative to buffer (in pixels, integer).
-- `buf_yoff`:   Vertical offset of ROI relative to buffer (in pixels, integer).
-- `buf_stride`: Bytes per line of an acquisition buffer (integer).
-- `buf_height`: Number of lines in an acquisition buffer (integer).
-- `buf_format`: Pixel format, *e.g.*, `PHX_DST_FORMAT_Y8` (integer).
-- `fps`:        Acquisition frame rate (in Hz, real).
-- `exposure`:   Exposure time (in seconds, real).
-- `gain`:       Analog gain (real).
-- `bias`:       Analog bias or black level (real).
-- `gamma`:      Gamma correction (real).
-- `blocking`:   Acquisition is blocking? (boolean)
-- `continuous`: Acquisition is continuous? (boolean)
+    ROI(xoff, yoff, width, height)
 
-Note: *integer* and *real* means that field has respectively integer (`Int`)
-and floating-point (`Float64`) value.
+or
 
-See also: [`setconfig!`](@ref), [`getconfig!`](@ref), [`fixconfig!`](@ref).
+    ROI(width, height)
+
+which assumes zero offsets.  Note that for efficiency reasons, there is no
+testing of the validity of the settings but you may use `checkroi` for that.
+
+See also: [`getroi`](@ref), [`setroi!`](@ref), [`checkroi`](@ref).
 
 """
 struct ROI
@@ -61,26 +49,10 @@ struct ROI
     yoff::Int
     width::Int
     height::Int
-    #cam_depth::Int
-    #buf_number::Int
-    #buf_xoff::Int
-    #buf_yoff::Int
-    #buf_stride::Int
-    #buf_height::Int
-    #buf_format::Int
-    #fps::Float64
-    #exposure::Float64
-    #gain::Float64
-    #bias::Float64
-    #gamma::Float64
-    #blocking::Bool
-    #continuous::Bool
     ROI(xoff::Integer, yoff::Integer, width::Integer, height::Integer) =
         new(xoff, yoff, width, height)
     ROI(width::Integer, height::Integer) =
         new(0, 0, width, height)
-    ROI() =
-        new(0, 0, typemax(Int), typemax(Int))
 end
 
 # Custom exception to report errors for non-extended methods.
@@ -88,8 +60,8 @@ struct NotImplementedException <: Exception
    sym::Symbol
 end
 
-# Colors (FIXME: use Julia package ColorTypes at https://github.com/JuliaGraphics/ColorTypes.jl)
-
+# Colors
+# (FIXME: use Julia package ColorTypes at https://github.com/JuliaGraphics/ColorTypes.jl)
 struct RGB{T}
     r::T
     g::T
